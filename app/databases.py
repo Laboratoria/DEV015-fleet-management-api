cd87bcd97fa8d137937d6a1f8c51f81975bc65a8
carolina <carolinartz5991@gmail.com>
2024-09-25 20:57:22 -0500

    feacture: change enviroment by config file

--- app/databases.py
+++ app/databases.py
@@ -1,13 +1,3 @@
-import os
-from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 
-# Cargar las variables de entorno desde el archivo .env
-load_dotenv()
-
-# Obtener la URL de la base de datos desde las variables de entorno
-arr_url = os.getenv('DATABASE')
-
 db = SQLAlchemy()
-def get_db():
-    return db
