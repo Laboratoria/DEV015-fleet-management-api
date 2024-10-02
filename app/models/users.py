a08f75a1a8e71b6cc32ae3861cabb100e8e7634f
carolina <carolinartz5991@gmail.com>
2024-10-01 21:28:58 -0500

    added user models to hashed password

---
+++ app/models/users.py
@@ -0,0 +1,40 @@
+import bcrypt
+from app.database.db import db
+
+class Users(db.Model):
+    """Class for table users"""
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    email = db.Column(db.String())
+    password = db.Column(db.String())
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = self.hash_password(password)  # Hash the password
+
+    def hash_password(self, password):
+        """Hashes the password using bcrypt"""
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_password.decode('utf-8')  # Store as a string in the database
+
+    def check_password(self, password):
+        """ comprueba si la contraseña ingresada hacae match con la cifrada"""
+        return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
+
+    def create(self):
+        """Añadir nuevo usuario a la tabla"""
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        """ Añadir tablas """
+        db.session.commit()
+
+    def delete(self):
+        """Borrar el usuario de la tabla """
+        db.session.delete(self)
+        db.session.commit()
