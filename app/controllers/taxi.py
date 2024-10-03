ecc70f6d5ee8fc7d43ba0a9601b5fb8230c0c5ee
carolina <carolinartz5991@gmail.com>
2024-10-03 09:35:55 -0500

    paylint fix

--- app/controllers/taxi.py
+++ app/controllers/taxi.py
@@ -1,10 +1,16 @@
+"""gestión de la tabal ade taxis
+"""
 from flask import jsonify
-from app.models.taxis import taxis
+from app.models.taxis import Taxi
 
 def get_taxis(page, limit):
-    taxis_query = taxis.query.paginate(page=page, limit=limit)
-    response = []
-    for taxis in taxis_query.items:
-        taxi_info = {"id": taxi.id, "plate": taxi.plate}
-        response.append(taxi_info)
+    """
+    retrona la paginacion de los taxis con informacion de la placa
+    """
+    taxis_paginated = Taxi.query.paginate(page=page, per_page=limit, error_out=False)
+    response= [
+        {"id": taxi.id, "plate": taxi.plate}
+        for taxi in taxis_paginated.items
+    ]
+
     return jsonify(response)
