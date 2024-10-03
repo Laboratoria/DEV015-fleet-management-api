26ec185f24a811c57fd479fde978c66631c7a3c2
carolina <carolinartz5991@gmail.com>
2024-10-03 15:58:48 -0500

    refactoring according to layered architecture

--- app/api.py
+++ app/api.py
@@ -1,61 +1,26 @@
 """
-definicion de rutas y maneja las solicitudes HTTP
+Módulo para crear la app
 """
-from flask import Flask, request
-#from flask_jwt_extended import JWTManager, get_jwt_identity
-
-from app.database.db import db
-from app.controllers.taxi import get_taxis
-from app.controllers.trajectories import select_trajectories
-from app.controllers.trajectories import select_last_location
-#falata la funcion de trayectoria con placa
-#importar funciones de users
-#importar login
-
+from flask import Flask
+from flask_jwt_extended import JWTManager  # Importación correcta
 from app.config import Config
-#importar encriptacion
-#importar herramientas de usuarios email
+from app.extensions import bcrypt, mail
+from app.routes import api_bp
+from app.database.db import db
 
 def create_app():
-    """Creates routes"""
+    """
+    Crea la aplicación Flask y configura las extensiones.
+    """
     app = Flask(__name__)
     app.config.from_object(Config)
+
     db.init_app(app)
+    bcrypt.init_app(app)
+    mail.init_app(app)
+    JWTManager(app)  # Elimina la variable `jwt` si no la necesitas
 
-    #bcrypt.init_app(app)  # Initializes bcrypt
-    #mail.init_app(app)  # Initializes flask-mail
-    #jwt = JWTManager(app)
-
-#primer endpoint
-    @app.route("/taxis", methods=["GET"])
-    #@token_required
-    def endp_taxis():
-        """Gets listas de taxis """
-        page = request.args.get("page", 1, type=int)
-        limit = request.args.get("limit", 10, type=int)
-        return get_taxis(page, limit)
-
-#segundo endpoint
-    @app.route("/trajectories", methods=["GET"])
-    #@token_required
-
-    def endp_trajectories():
-        """ get la localizacion de un taxi por fechas"""
-        taxi_id = request.args.get("taxiId")
-        date = request.args.get("date")
-        return select_trajectories(taxi_id, date)
-
-#tercer endpoint
-    @app.route("/trajectories/latest", methods=["GET"])
-    #@token_required
-    def get_last_location():
-        """Gets la ultima localizacion de cada taxi"""
-        page = request.args.get("page", 1, type=int)
-        limit = request.args.get("limit", 10, type=int)
-        return select_last_location(page, limit)
-
-
-
-#cuarto endopoint users
+    # Registra las rutas
+    app.register_blueprint(api_bp)
 
     return app
