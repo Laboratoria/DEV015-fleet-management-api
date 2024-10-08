e5a45cc541d5284b0635a0636895d46c3fc807ea
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2024-10-08 17:50:30 -0500

    Modificar endpoint para consultar base de datos

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
 # Para poder trabajar con las variables de entorno
-import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+import os 
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy 
 # Importa load_dotenv de la librería de python-dotenv, para cargar las variables de entorno
 from dotenv import load_dotenv
 
 #Cargar variable de entorno
-load_dotenv(dotenv_path='.env.development.local')
+load_dotenv(dotenv_path='.env.development.local') 
 
 app = Flask(__name__)
 
 # Se carga la variable de entorno
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL') 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 #Crea una instancia de SQLAlchemy y la vincula con Flask
-db = SQLAlchemy(app)
+db = SQLAlchemy(app) 
 
-# Aquí se define el endpoint y responde a la petición 'GET'
+# Modelo
+class Taxi(db.Model):
+    __tablename__ = 'taxis'
+
+    id = db.Column(db.Integer, primary_key = True)
+    plate = db.Column(db.String(20), nullable=False)
+
+    #Método
+    def dictionary(self):
+        return{
+            'id': self.id,
+            'plate': self.plate
+        }
+
+# Endpoint: responde a la petición 'GET'
 @app.route('/taxis', methods=['GET'])
 def get_taxis():
-    return "Hola mundo", 200
+    #return "Hola mundo", 200
+    taxis = Taxi.query.all()
+
+    return jsonify([taxi.dictionary() for taxi in taxis]), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
