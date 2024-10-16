45d617b0db703dfe863f4cf8bebd9baa1de2d2f4
mabel-gp <167148030+mabel-gp@users.noreply.github.com>
2024-10-15 19:15:22 -0500

    Modificar para test endpoint /taxis

--- src/route.py
+++ src/route.py
@@ -11,7 +11,7 @@ def get_taxis():
     page = request.args.get('page', default=1, type=int)
     limit = request.args.get('limit', default=10, type=int)
 
-    if page > 1 or limit > 10:
+    if page < 1 or limit < 1:
         return jsonify({'error': 'Bad Request: Page or limit is not valid'}), 400
 
     database_query = Taxi.query #Realiza consulta en la tabla Taxis
@@ -19,7 +19,7 @@ def get_taxis():
     if plate:
         filtered_query = database_query.filter(Taxi.plate.like('%'+plate+'%'))
     else:
-        filtered_query = database_query.filter(Taxi.plate == '')
+        filtered_query = database_query
 
     taxis = filtered_query.paginate(page=page, per_page=limit) #Paginar los resultados  
         
